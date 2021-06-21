200e72f0bc66d126c4d6d1ae2ac5a15fd4de6eca
黄志茂 <huangzhimao@iyunbao.com>
2021-06-21 16:56:11 +0800

    init

---
+++ src/flower/utils.js
@@ -0,0 +1,4 @@
+// 获取1-10随机整数
+export function getInit() {
+    return Math.round(Math.random() * 9);
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Flower from './flower/index'
+import './index.scss';
+
+ReactDOM.render(
+  <Flower />,
+  document.getElementById('root')
+);
