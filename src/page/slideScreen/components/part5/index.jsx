078fd32a8d0c6fe6282e98e45675408abe0c908b
黄志茂 <huangzhimao@iyunbao.com>
2021-06-23 15:28:45 +0800

    feat:..

--- src/page/slideScreen/components/part5/index.jsx
+++ src/page/slideScreen/components/part5/index.jsx
@@ -1,13 +1,21 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import CommonPart from '../commonPart/index';
 import './index.scss';
 
-
-const Part = ({onClose}) => {
+import data from './data'
+const Part = ({ onClose }) => {
     return (
         <CommonPart onClose={onClose}>
             <div className="part5-container">
-               
+                <div className="text-box">
+                    {
+                        data.map((item, index) => {
+                            return (
+                                <p key={index}>{item}</p>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </CommonPart>
     )
--- src/page/slideScreen/index.jsx
+++ src/page/slideScreen/index.jsx
@@ -1,4 +1,5 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './index.scss';
 import bgImage from '../../assets/slideScreen/slide/0.jpg';
 import Part1 from './components/part1/index.jsx';
@@ -14,102 +15,119 @@ import img4 from '../../assets/slideScreen/slide/4.png';
 import img5 from '../../assets/slideScreen/slide/5.png';
 
 //总长度
-const totalWidth=12000;
+const totalWidth = 12000;
 //起始左边距离
-const startMargin=1230;
+const startMargin = 1230;
 //每次移动距离
 const moveDistance = 2000;
 //每个发展历程之间的间距
-const lineItemMargin=400;
+const lineItemMargin = 400;
 //每次移动的时间
-const moveTime=10000
+const moveTime = 2000
 
-const partMap={
-    part1:Part1,
-    part2:Part2,
-    part3:Part3,
-    part4:Part4,
-    part5:Part5
+const partMap = {
+    part1: Part1,
+    part2: Part2,
+    part3: Part3,
+    part4: Part4,
+    part5: Part5
 }
+
+function proxyRequest() {
+    axios({
+        method: 'post',
+        url: "http://192.168.1.177:8001",
+        data: {
+            action: "move_to_next"
+        }
+    }).then((resp) => {
+        console.log(resp);
+    }, (err) => {
+        console.log(err);
+    });
+}
+
 class SlideScreen extends Component {
     state = {
-        left:0,
-        index:0,
-        show:false,
-        lock:false
+        left: 0,
+        index: 0,
+        show: false,
+        lock: false
     }
 
-   move=(direction)=>{
-    const {left,index,lock} = this.state;
-    let moveDis = moveDistance
-    
-    if(lock)return;
-    if(direction==='left'){
-        if(index!==0){
-            this.setState({
-                left: left+moveDis,
-                index:index-1,
-                lock:true
-            })
-        }
-        if(index===1){
-            this.setState({
-                lock:false,
-                show:false
-            })
-        }
-    }else{
-        if(index<5){
-            this.setState({
-                left: left-moveDis,
-                index:index+1,
-                lock:true
-            }) 
-        }
-    }
-    setTimeout(()=>{
-        if(!(direction === 'left' && index===1)){
-            this.setState({
-                show:true,
-            })  
-        }
-    },moveTime+500)
+    move = (direction) => {
+        const { left, index, lock } = this.state;
+        let moveDis = moveDistance;
 
-   }
+        if (lock) return;
+        if (direction === 'left') {
+            if (index !== 0) {
+                this.setState({
+                    left: left + moveDis,
+                    index: index - 1,
+                    lock: true
+                })
+                proxyRequest()
+            }
+            if (index === 1) {
+                this.setState({
+                    lock: false,
+                    show: false
+                })
+            }
+        } else {
+            if (index < 5) {
+                this.setState({
+                    left: left - moveDis,
+                    index: index + 1,
+                    lock: true
+                })
+                proxyRequest()
+            }
+        }
+        setTimeout(() => {
+            if (!(direction === 'left' && index === 1)) {
+                this.setState({
+                    show: true,
+                })
+            }
+        }, moveTime + 500)
+
+    }
     render() {
-        const {left ,index,show} = this.state;
+        const { left, index, show } = this.state;
         const PartComponent = partMap[`part${index}`]
-        const marginStyle = {marginRight:`${lineItemMargin}px`}
-        
+        const marginStyle = { marginRight: `${lineItemMargin}px` }
+
         return (
-           <div className="slide-screen-page">
-               <div 
-                className="bg-img-box"
-                style={{
-                    transform: `translateX(${left}px)`,
-                    width:`${totalWidth}px`,
-                    transition: `all ${moveTime/1000}s linear`
-                }}
-               >
-                    <img className="bg" src={bgImage}/>
+            <div className="slide-screen-page">
+                <div
+                    className="bg-img-box"
+                    style={{
+                        transform: `translateX(${left}px)`,
+                        width: `${totalWidth}px`,
+                        transition: `all ${moveTime / 1000}s linear`
+                    }}
+                >
+                    <img className="bg" src={bgImage} />
                     <div className="time-line">
                         <div className="throule-line"></div>
-                        <img className="line-1"  style={{...marginStyle,marginLeft:startMargin}} src={img1}/>
-                        <img className="line-2"  style={marginStyle} src={img2}/>
-                        <img className="line-3"  style={marginStyle} src={img3}/>
-                        <img className="line-4"  style={marginStyle} src={img4}/>
-                        <img className="line-5"  style={marginStyle} src={img5}/>
-                        
+                        <img className="line-1" style={{ ...marginStyle, marginLeft: startMargin }} src={img1} />
+                        <img className="line-2" style={marginStyle} src={img2} />
+                        <img className="line-3" style={marginStyle} src={img3} />
+                        <img className="line-4" style={marginStyle} src={img4} />
+                        <img className="line-5" style={marginStyle} src={img5} />
+
                     </div>
-                   
-               </div>
-                <div className="left-control" onClick={()=>{this.move('left')}}></div>
-                <div className="right-control" onClick={()=>{this.move('right')}}></div>
-               {
-                   (show && index>0) && <PartComponent onClose={()=>{this.setState({show: false,lock:false})}}/>
-               } 
-           </div>
+
+                </div>
+                <div className="left-control" onClick={() => { this.move('left') }}></div>
+                <div className="right-control" onClick={() => { this.move('right') }}></div>
+                {
+                    (show && index > 0) && <PartComponent onClose={() => { this.setState({ show: false, lock: false }) }} />
+                }
+            </div>
         )
     }
 }
-export default SlideScreen 
+export default SlideScreen
